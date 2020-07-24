5f574e505797369d20022e6f705ebb7c499126b0
jinpk <jinpk@qq.com>
2020-07-24 19:09:58 +0800

    v1.0.2：jinun

--- springTest/src/main/java/com/offcn/SpringTest01.java
+++ springTest/src/main/java/com/offcn/SpringTest01.java
@@ -9,6 +9,10 @@ public class SpringTest01 {
     //1.0.1
     public static void main(String[] args) {
         SpringApplication.run(SpringTest01.class,args);
+
+        //askjdfoiajsplo ihjq
+
+
     }
 
 
